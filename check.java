dd0254f72fbcabb49b5ae1c75f1d2b703d75f461
MKCE_Product <gkam.prem89@gmail.com>
2017-09-02 09:32:09 +0530

    HI

---
+++ check.java
@@ -0,0 +1,13 @@
+class check
+{
+public static void main(String[] args)
+{
+int input;
+Scanner sc = new Scanner(System.in);
+input = sc.nextInt();
+if(input>1)
+System.out.print(0);
+else
+System.out.print(input);
+}
+}
